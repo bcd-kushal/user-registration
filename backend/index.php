06c6baf4f9351eb3ab471512960d86c2ad15889f
liumstone <am.kushal02@gmail.com>
2023-11-02 03:34:25 +0530

    Add files via upload

---
+++ backend/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="./frontImg.jpg">
+    <iframe src="./header.html" frameborder="0" height="54px" width="1920px"></iframe>
+
+
+
+    <table>
+        <tr>
+            <td width="150px"></td>
+            <td width="550px">
+                
+
+
+
+
+                <form action="" method="post">
+
+                    <br><br><br><br>
+                    <br><br><br><br>
+                    Your &nbsp;
+                    <input type="text" name="" id="" placeholder="First Name" tabindex="1" required>
+                    &nbsp; and &nbsp;
+                    <input type="text" name="" id="" placeholder="Last Name" tabindex="2" required>
+                    <br>
+                    <br>
+                    <hr>
+                    <br>
+                    Do mention your &nbsp;
+                    <input type="tel" name="" id="" placeholder="Mobile" tabindex="3" required>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; or &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="email" name="" id="" placeholder="Email ID" tabindex="4" required>
+                    <br>
+                    <br>
+                    <hr>
+                    <br>
+                    How would you rate our services? &nbsp;
+                    <select name="" id="">
+                        <option value="rate1">5 - Satisfied</option>
+                        <option value="rate1">4 - Great</option>
+                        <option value="rate1">3 - Alright</option>
+                        <option value="rate1">2 - Mediocre</option>
+                        <option value="rate1">1 - Poor</option>
+                    </select>
+                    &nbsp; out of 5
+                    <br>
+                    <br>
+                    <hr>
+                    <br>
+                    We would love hearing back from you!! 
+                    <br>
+                    <br>
+                    <textarea name="" id="" cols="50" rows="7" placeholder="Drop your feedback here!"></textarea>
+                    
+                    <br>
+                    <br>
+                    <hr>
+                    <br>
+                    <button type="submit" tabindex="5">Submit Response</button> 
+                    &nbsp; || &nbsp; 
+                    <button type="reset" tabindex="6">Reset Values</button>
+                    
+                    <br><br><br><br>
+
+                    
+    <?php
+        echo "HELLO DEAR ME.";
+    ?>
+
+
+                    <br><br><br><br>
+                    <br><br><br><br>
+                    
+
+                </form>
+
+
+
+
+
+
+            </td>
+            <td width="200px">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <img src="./noBG.png" alt="">
+            </td>
+            <td align="right" width="550px">
+                <font size="9">
+                    <h1>We Value Your <br>Feedback!</h1>
+                </font>
+            </td>
+        </tr>
+    </table>
+
+
+    <iframe src="./footer.html" frameborder="0" height="454px" width="1920px"></iframe>
+
+
+
+</body>
+</html>
---
+++ backend/new copy.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./design.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form_box">
+
+
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+            
+        
+        
+        
+        
+            </form>
+
+
+        </div>
+        <div class="bgBox"></div>
+    </div>
+</body>
+</html>
